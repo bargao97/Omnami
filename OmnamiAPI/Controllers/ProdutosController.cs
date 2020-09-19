7dbc97100fac197c8aee8f47095927984027f733
gabriel bargas <gabriel.bargas97@hotmail.com>
2020-09-18 21:21:49 -0300

    Subindo produtos

---
+++ OmnamiAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OmnamiAPI.Database;
+
+namespace OmnamiAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produtos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produtos>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        // GET: api/Produtos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produtos>> GetProdutos(int id)
+        {
+            var produtos = await _context.Produtos.FindAsync(id);
+
+            if (produtos == null)
+            {
+                return NotFound();
+            }
+
+            return produtos;
+        }
+
+        // PUT: api/Produtos/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProdutos(int id, Produtos produtos)
+        {
+            if (id != produtos.IdProduto)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produtos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produtos
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Produtos>> PostProdutos(Produtos produtos)
+        {
+            _context.Produtos.Add(produtos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProdutos", new { id = produtos.IdProduto }, produtos);
+        }
+
+        // DELETE: api/Produtos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Produtos>> DeleteProdutos(int id)
+        {
+            var produtos = await _context.Produtos.FindAsync(id);
+            if (produtos == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produtos);
+            await _context.SaveChangesAsync();
+
+            return produtos;
+        }
+
+        private bool ProdutosExists(int id)
+        {
+            return _context.Produtos.Any(e => e.IdProduto == id);
+        }
+    }
+}
